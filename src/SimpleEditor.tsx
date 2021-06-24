4a8568d584c41ed0062a06c4961965c1139540a5
ui3o.com <ui3o.com@gmail.com>
2021-06-24 23:00:06 +0200

    not saved warning before migration

--- src/SimpleEditor.tsx
+++ src/SimpleEditor.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
-import { Button } from '@grafana/ui';
 import { StandardEditorProps } from '@grafana/data';
+import { Icon, Modal, ToolbarButton } from '@grafana/ui';
+import { Dashboard } from 'dto/dashboard.dto';
 import { QuickLogConfigure } from 'QuickLogConfigure';
+import React from 'react';
 
 export const SimpleEditor: React.FC<StandardEditorProps<boolean>> = ({ value, onChange, item, context }) => {
   const [configureOpen, setConfigureOpen] = React.useState<boolean>();
+  const [modalVisible, setModalVisible] = React.useState<boolean>();
 
   console.log(item, context);
+
+  const getDashboard = async () => {
+    const _uid = location.pathname.split('/d/')[1].split('/')[0];
+    const _panelId = Number(new URLSearchParams(window.location.search).get('editPanel'));
+    const _boardProp = await fetch(`./api/dashboards/uid/${_uid}`);
+    const dashboard: Dashboard = await _boardProp.json();
+    const _panel = dashboard.dashboard.panels.filter((p) => p.id === _panelId)[0];
+    if (JSON.stringify(_panel.options) !== JSON.stringify(context.options)) {
+      setModalVisible(true);
+    } else {
+      setConfigureOpen(true);
+    }
+  };
+
   return (
     <div>
-      <Button
-        onClick={() => {
-          setConfigureOpen(true);
-        }}
+      <ToolbarButton
+        icon="share-alt"
+        tooltip="Save changes before migrate the current settings"
+        variant="primary"
+        onClick={getDashboard}
       >
         Migrate
-      </Button>
+      </ToolbarButton>
+      <Modal
+        title={
+          <div className="modal-header-title">
+            <Icon name="exclamation-triangle" size="lg" />
+            <span className="p-l-1">Warning</span>
+          </div>
+        }
+        onDismiss={() => setModalVisible(false)}
+        isOpen={modalVisible}
+      >You have unsaved changes. Please save your dashboard changes before migration!</Modal>
       {configureOpen && <QuickLogConfigure onClose={() => setConfigureOpen(false)} context={context} />}
     </div>
   );
